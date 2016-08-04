3728df0c561ec1534e0749f7e938ef08e8dd2a0f
elaine <elaine-me@elaines-mbp.attlocal.net>
2016-08-04 14:52:11 -0700

    changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# maps
+MEAN stack app example using google geocoder API
+<br><br>	•	Add your mlab URI to server.js 
+<br>	•	Add your google maps API key inside script tag to index.html
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
-var MONGOLAB_URI = 'YOUR-MONGO-URI-HERE';
+var MONGOLAB_URI = 'mongodb://outerlimits:mercy@ds015713.mlab.com:15713/reaction';
 var uristring = 'mongodb://localhost:27017/maps';
 mongoose.connect(MONGOLAB_URI, function() {
     console.log('Mongoose connected to your soul');
